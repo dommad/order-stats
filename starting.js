b3b1c743dbdc27772df6a5caccc749de2d952663
dommad <dmadej@ust.hk>
2022-05-15 11:59:05 +0800

    This is my first commmit

---
+++ starting.js
@@ -0,0 +1,9 @@
+// this script used to check if the number is even or odd
+
+const num = 6;
+
+const result = (num % 2  != 0) ? "odd" : "even";
+
+// display the result
+
+console.log(`Number is ${result}.`);
